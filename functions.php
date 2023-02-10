9f3366706c50a779189f947cd0aba3ea6745a366
Juaneswfr <fullstack@belkacompany.com>
2023-02-10 11:00:33 -0500

    fixing cositas

--- functions.php
+++ functions.php
@@ -165,22 +165,13 @@ function mokaru_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'mokaru_scripts' );
 
-// Incluir Bootstrap JS
-function bootstrap_js() {
-	wp_enqueue_script( 'bootstrap_js', get_stylesheet_directory_uri() . '/assets/bootstrap.min.js', array('jquery'), '', true); 
-}
-add_action( 'wp_enqueue_scripts', 'bootstrap_js');
-// Incluir Bootstrap CSS
-function bootstrap_css() {
-	wp_enqueue_style( 'bootstrap_css', get_stylesheet_directory_uri() . '/assets/bootstrap.min.css', array(), ''); 
-}
-add_action( 'wp_enqueue_scripts', 'bootstrap_css');
-
 // Incluir Bootstrap CSS
 function custom_scripts() {
+	wp_enqueue_style( 'bootstrap_css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', array(), ''); 
+	wp_enqueue_script( 'bootsrap_js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true); 
 	wp_enqueue_style( 'custom_css', 	get_stylesheet_directory_uri() . '/assets/custom.css', 	array(), ''); 
 	wp_enqueue_script( 'custom_js', get_stylesheet_directory_uri() . '/assets/js/custom.js', array('jquery'), '', true); 
-
+	
 	if ( is_page_template( 'templates/template-checkout.php' ) ) {
 		wp_enqueue_style( 'main_css', get_stylesheet_directory_uri() .  '/assets/checkout/checkout.css',array(), '' );
 		wp_enqueue_script( 'main_js', get_stylesheet_directory_uri() . '/assets/checkout/checkout.js', array('jquery'), '', true); 
